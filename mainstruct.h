4b77eaeb0ce94d0e5f29df56a8d06aae867c093b
adriangrajdeanu95 <grajdeanuadrian@gmail.com>
2017-02-23 20:41:26 +0200

    first structures

---
+++ mainstruct.h
@@ -0,0 +1,13 @@
+    struct req_description{
+        int video;
+        int endpoint;
+        int requests;
+        int total_lat; //va fi calculat la citire
+        };
+
+    struct cache{
+        int videos[10000];
+        size_left;
+        endpoints[1000];
+        };
+
